a7adc0b3e241c49c2a6145a88543c16e3f2645ed
David Gao <david.04.gao@gmail.com>
2023-11-08 00:22:34 -0500

    proof api works

--- project2/script.js
+++ project2/script.js
@@ -1,2 +1,23 @@
-const p = document.querySelector("p");
-let URL = "https://api.jikan.moe/v4/";
+window.onload = () => {
+    testing();
+};
+
+function testing(){
+    const p = document.querySelector("#hi");
+    var apiToken = ' 89abe689-ce3d-4035-acfd-65d442782f72 ';
+    var apiEndpointPath = 'subjects/44';
+    var requestHeaders =
+      new Headers({
+        Authorization: 'Bearer ' + apiToken,
+      });
+    var apiEndpoint =
+      new Request('https://api.wanikani.com/v2/' + apiEndpointPath, {
+        method: 'GET',
+        headers: requestHeaders
+      });
+    
+    fetch(apiEndpoint)
+      .then(response => response.json())
+      .then(responseBody => p.innerHTML = responseBody.data.meaning_mnemonic
+      );
+}
