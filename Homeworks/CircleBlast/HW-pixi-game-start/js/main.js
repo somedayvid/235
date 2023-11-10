bba7f5c93b9318d705797b064399c008008b580c
David Gao <david.04.gao@gmail.com>
2023-11-10 02:31:59 -0500

    update

--- Homeworks/CircleBlast/HW-pixi-game-start/js/main.js
+++ Homeworks/CircleBlast/HW-pixi-game-start/js/main.js
@@ -9,7 +9,7 @@ document.body.appendChild(app.view);
 
 // constants
 const sceneWidth = app.view.width;
-const sceneHeight = app.view.height;	
+const sceneHeight = app.view.height;
 
 // pre-load the images (this code works with PIXI v6)
 app.loader.
@@ -42,7 +42,7 @@ let stage;
 
 // game variables
 let startScene;
-let gameScene,ship,scoreLabel,lifeLabel,shootSound,hitSound,fireballSound;
+let gameScene, ship, scoreLabel, lifeLabel, shootSound, hitSound, fireballSound;
 let gameOverScene;
 
 let circles = [];
@@ -56,24 +56,29 @@ let levelNum = 1;
 let paused = true;
 
 function setup() {
-	stage = app.stage;
-	// #1 - Create the `start` scene
-	startScene = new PIXI.Container();
+    stage = app.stage;
+    // #1 - Create the `start` scene
+    startScene = new PIXI.Container();
     stage.addChild(startScene);
-	// #2 - Create the main `game` scene and make it invisible
+
+    // #2 - Create the main `game` scene and make it invisible
     gameScene = new PIXI.Container();
     gameScene.visible = false;
     stage.addChild(gameScene);
-	// #3 - Create the `gameOver` scene and make it invisible
-	gameOverScene = new PIXI.Container();
+
+    // #3 - Create the `gameOver` scene and make it invisible
+    gameOverScene = new PIXI.Container();
     gameOverScene.visible = false;
     stage.addChild(gameOverScene);
-	// #4 - Create labels for all 3 scenes
-	createLabelsAndButtons();
-	// #5 - Create ship
+
+    // #4 - Create labels for all 3 scenes
+    createLabelsAndButtons();
+
+    // #5 - Create ship
     ship = new Ship();
     gameScene.addChild(ship);
-	// #6 - Load Sounds
+
+    // #6 - Load Sounds
     shootSound = new Howl({
         src: ['sounds/shoot.wav']
     });
@@ -85,31 +90,34 @@ function setup() {
     fireballSound = new Howl({
         src: ['sounds/fireball.mp3']
     });
-	
-	// #7 - Load sprite sheet
-		
-	// #8 - Start update loop
-	
-	// #9 - Start listening for click events on the canvas
-	
-	// Now our `startScene` is visible
-	// Clicking the button calls startGame()
+
+    // #7 - Load sprite sheet
+    explosionTextures = loadSpriteSheet();
+
+    // #8 - Start update loop
+    app.ticker.add(gameLoop);
+
+    // #9 - Start listening for click events on the canvas
+    app.view.onclick = fireBullet;
+
+    // Now our `startScene` is visible
+    // Clicking the button calls startGame()
 }
 
-function createLabelsAndButtons(){
+function createLabelsAndButtons() {
     let buttonStyle = new PIXI.TextStyle({
         fill: 0xFF0000,
         fontSize: 48,
-        fontFamily: "Verdana"
+        fontFamily: "Futura"
     });
 
-    //1 setup startscene
-    //1a make top start lavel
+    // 1 - set up 'startscene"
+    // 1A - make top start label
     let startLabel1 = new PIXI.Text("Circle Blast!");
     startLabel1.style = new PIXI.TextStyle({
         fill: 0xFFFFFF,
         fontSize: 96,
-        fontFamily: "Verdana",
+        fontFamily: "Futura",
         stroke: 0xFF0000,
         strokeThickness: 6
     });
@@ -117,12 +125,12 @@ function createLabelsAndButtons(){
     startLabel1.y = 120;
     startScene.addChild(startLabel1);
 
-    //1B make middle start label
-    let startLabel2 = new PIXI.Text("R U worthy ..?");
+    // 1B - make middle start label
+    let startLabel2 = new PIXI.Text("R U worthy..?");
     startLabel2.style = new PIXI.TextStyle({
         fill: 0xFFFFFF,
         fontSize: 32,
-        fontFamily: "Verdana",
+        fontFamily: "Futura",
         fontStyle: "italic",
         stroke: 0xFF0000,
         strokeThickness: 6
@@ -130,28 +138,29 @@ function createLabelsAndButtons(){
     startLabel2.x = 185;
     startLabel2.y = 300;
     startScene.addChild(startLabel2);
-    
-    //1C make start game button 
-    let startButton = new PIXI.Text("Enter, ... if you dare!");
+
+    // 1C - make start game button
+    let startButton = new PIXI.Text("Enter, .. if you dare!");
     startButton.style = buttonStyle;
     startButton.x = 80;
     startButton.y = sceneHeight - 100;
     startButton.interactive = true;
     startButton.buttonMode = true;
-    startButton.on("pointerup",startGame);
-    startButton.on("pointerover", e => e.target.alpha = 0.7);
-    startButton.on("pointerour", e => e.currentTarget.alpha = 1.0);
+    startButton.on("pointerup", startGame); // startGame is a function reference
+    startButton.on('pointerover', e => e.target.alpha = 0.7); // concise arrow function with no brackets 
+    startButton.on('pointerout', e => e.currentTarget.alpha = 1.0); // ditto startscene. addChild(startButton);
     startScene.addChild(startButton);
 
-    //2 set up gameScene
+    // 2 - set up gameScene
     let textStyle = new PIXI.TextStyle({
         fill: 0xFFFFFF,
         fontSize: 18,
-        fontFamily: "Verdana",
+        fontFamily: "Futura",
         stroke: 0xFF0000,
         strokeThickness: 4
     });
-    //2a make score label
+
+    // 2A - make score label
     scoreLabel = new PIXI.Text();
     scoreLabel.style = textStyle;
     scoreLabel.x = 5;
@@ -159,55 +168,82 @@ function createLabelsAndButtons(){
     gameScene.addChild(scoreLabel);
     increaseScoreBy(0);
 
-    //2b make life label
+    // 2B - make life label
     lifeLabel = new PIXI.Text();
     lifeLabel.style = textStyle;
     lifeLabel.x = 5;
     lifeLabel.y = 26;
     gameScene.addChild(lifeLabel);
-    decreaseLifeBy(0);
+    decreaselifeBy(0);
 
-    //3 set up gameOverScene
-    //3a make game over text
-    let gameOverText = new PIXI.Text("Game Over! \n         :-0");
-    textStyle = new PIXI.textStyle({
+    // 3 - set up `gameOverScene`
+    // 3A - make game over text
+    let gameOverText = new PIXI.Text("Game Over!\n        :-O");
+    textStyle = new PIXI.TextStyle({
         fill: 0xFFFFFF,
         fontSize: 64,
-        fontFamily: "Verdana",
+        fontFamily: "Futura",
         stroke: 0xFF0000,
         strokeThickness: 6
     });
     gameOverText.style = textStyle;
     gameOverText.x = 100;
-    gameOverText.y = sceneHeight/2 - 160;
+    gameOverText.y = sceneHeight / 2 - 160;
     gameOverScene.addChild(gameOverText);
 
-    //3b make play again button
+    // 3B - make "play again?" button
     let playAgainButton = new PIXI.Text("Play Again?");
     playAgainButton.style = buttonStyle;
     playAgainButton.x = 150;
     playAgainButton.y = sceneHeight - 100;
     playAgainButton.interactive = true;
     playAgainButton.buttonMode = true;
-    playAgainButton.on("pointerup",startGame); // startGame is a function reference
-    playAgainButton.on('pointerover',e=>e.target.alpha = 0.7); // concise arrow function with no brackets
-    playAgainButton.on('pointerout',e=>e.currentTarget.alpha = 1.0); // ditto
+    playAgainButton.on("pointerup", startGame); // startGame is a function reference
+    playAgainButton.on('pointerover', e => e.target.alpha = 0.7); // concise arrow function with no brackets
+    playAgainButton.on('pointerout', e => e.currentTarget.alpha = 1.0); // ditto
     gameOverScene.addChild(playAgainButton);
 }
 
-function startGame(){
+function startGame() {
     startScene.visible = false;
     gameOverScene.visible = false;
     gameScene.visible = true;
 }
 
-function increaseScoreBy(value){
+function increaseScoreBy(value) {
     score += value;
     scoreLabel.text = `Score ${score}`;
 }
 
-function decreaseLifeBy(value){
+function decreaselifeBy(value) {
     life -= value;
     life = parseInt(life);
-    lifeLabel.text = `Life ${life}%`;
+    lifeLabel.text = `life ${life}`;
+}
+
+function gameLoop(){
+	// if (paused) return; // keep this commented out for now
+	
+	// #1 - Calculate "delta time"
+	let dt = 1/app.ticker.FPS
+	
+	// #2 - Move Ship
+	
+	
+	// #3 - Move Circles
+	
+	
+	// #4 - Move Bullets
+
+	
+	// #5 - Check for Collisions
+	
+	
+	// #6 - Now do some clean up
+	
+	
+	// #7 - Is game over?
+	
+	
+	// #8 - Load next level
 }
