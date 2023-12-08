0f4632b559d52e04412d08d7044db9d6525c8058
David Gao <david.04.gao@gmail.com>
2023-12-08 09:59:38 -0500

    commit

--- project3/main.js
+++ project3/main.js
@@ -3,46 +3,93 @@
 let drum, twang, guitarC;
 let monsters;
 let thumper, boomer, dunner;
+let currentMonster;
 
 let sounds;
+
 document.addEventListener("DOMContentLoaded", function() {
 
 window.onload = (e) => {
-    const slider = document.querySelector("#interval");
-    slider.oninput = change;
+    const intervalSlider = document.querySelector("#interval");
+    const speedSlider = document.querySelector("#rate");
+    const volumeSlider = document.querySelector("#volume");
+    const loopSwitch = document.querySelector("#loop");
+
+    const intervalnum = document.querySelector("#intervalnum");
+    const volumenum = document.querySelector("#volumenum");
+    const ratenum = document.querySelector("#ratenum");
+
     setup();
     createMonsters();
+
+    currentMonster = monsters[0];
+
+    intervalnum.innerHTML = intervalSlider.value;
+    volumenum.innerHTML = volumeSlider.value;
+    ratenum.innerHTML = speedSlider.value;
+
+    intervalSlider.oninput = changeMonsterStats;
+    speedSlider.oninput = changeMonsterStats;
+    volumeSlider.oninput = changeMonsterStats;
+    loopSwitch.onclick = changeMonsterStats;
+
 }
 
-function change(){
-    const sliderNum = document.querySelector("#intervalnum");
-    sliderNum.innerHTML = document.querySelector("#interval").value;
+function changeMonsterStats(){
+    if(this.id != "loop"){
+        document.querySelector(`#${this.id + "num"}`).innerHTML = this.value;
+    }
+    for(let i = 0; i < monsters.length; i++){
+        if(currentMonster.id == monsters[i].name){
+            switch(this.id){
+                case "interval":
+                    monsters[i].interval = Number(this.value);
+                    break;
+                case "rate":
+                    monsters[i].rate = Number(this.value);
+                    break;
+                case "volume":
+                    monsters[i].volume = Number(this.value);
+                    break;
+                case "loop":
+                    console.log(this);
+                    if(this.value == "true"){
+                        monsters[i].loop = false;
+                        this.value = false;
+                    }
+                    else if(this.value == "false"){
+                        monsters[i].loop = true;
+                        this.value = true;
+                    }
+                    break;
+            }
+        }
+    }
 }
 
 function setup(){
     sounds = [
         drum = new Howl({
             src:["Sounds/DrumBoop.mp3"],
+            volume: 1.0,
+            rate: 1.0
         }),
         twang = new Howl({
-            src:["Sounds/Twang.mp3"]
+            src:["Sounds/Twang.mp3"],
+            volume: 1.0,
+            rate: 1.0
         }),
         guitarC = new Howl({
-            src:["Sounds/GuiterC.wav"]
+            src:["Sounds/GuiterC.wav"],
+            volume: 1.0,
+            rate: 1.0
         })
     ]
-
     monsters = [
-        thumper = {name: "thumper", interval: 1, intervalID: "",playing: false, rate: 1.0, sound: drum, soundID: getID(drum)},
-        boomer = {name: "boomer", interval: 2, intervalID:"", playing: false, rate: 1.0, sound: twang, soundID: getID(twang)},
-        dunner = {name: "dunner", interval: 1, intervalID:"", playing: false, rate: 1.0, sound: guitarC, soundID: getID(guitarC)}
+        thumper = {name: "thumper", interval: 1, intervalID: "",playing: false, loop:true, volume:1.0, rate: 1.0, sound: drum},
+        boomer = {name: "boomer", interval: 1, intervalID:"", playing: false, loop:true, volume:1.0, rate: 1.0, sound: twang},
+        dunner = {name: "dunner", interval: 1, intervalID:"", playing: false, loop:true, volume:1.0, rate: 1.0, sound: guitarC}
     ];
-}
-
-function getID(sound){
-    const id = sound.play();
-    sound.stop();
-    return id;
 }
 
 function createMonsters(){
@@ -51,6 +98,8 @@ function createMonsters(){
         newItem.className = "monster";
         newItem.id = monsters[i].name;
         newItem.onclick = toggler;
+        newItem.addEventListener("mouseover", changeBoxDisplayInfo); 
+        newItem.addEventListener("mouseover", changeMon); 
         document.querySelector("#display").appendChild(newItem);
     }
 }
@@ -59,23 +108,57 @@ function toggler(){
     let audio;
     for(let i = 0; i< monsters.length;i++){
         if(this.id == monsters[i].name){
-            const item = document.querySelector(`#${monsters[i].name}`);
-            audio = monsters[i].sound;
-            if(monsters[i].playing == false){
+            const monster = monsters[i];
+            const item = document.querySelector(`#${monster.name}`);
+            audio = monster.sound;
+            if(monster.playing == false){
+                audio.rate(monster.rate);
+                audio.volume(monster.volume);
+                monster.playing = true;
                 audio.play();
-                monsters[i].intervalID = setInterval(playSound, monsters[i].interval * 1000, audio);
-                monsters[i].playing = true;
-                item.style.transform = 'translateY(4px)';
-                item.style.boxShadow = '0 5px #666';
+                if(monster.loop){
+                    monster.intervalID = setInterval(playSound, monsters[i].interval * 1000, audio);
+                    item.style.transform = 'translateY(4px)';
+                    item.style.boxShadow = '0 5px #666';
+                }
             }
-            else if(monsters[i].playing == true){
-                clearInterval(monsters[i].intervalID);
-                monsters[i].playing = false;
-                item.style.transform = 'none';
-                item.style.boxShadow = '0 9px #999';
+            else if(monster.playing == true){
+                if(monster.loop){
+                    clearInterval(monster.intervalID);
+                    item.style.transform = 'none';
+                    item.style.boxShadow = '0 9px #999';
+                }
+                monster.playing = false;
             }
         }
     }
+}
+
+function changeBoxDisplayInfo(){
+    for(let i = 0; i < monsters.length;i++){
+        if(this.id == monsters[i].name){
+            const monster = monsters[i];
+            const intervalSlider = document.querySelector("#interval");
+            const speedSlider = document.querySelector("#rate");
+            const volumeSlider = document.querySelector("#volume");
+            const loopSwitch = document.querySelector("#loop");
+
+            document.querySelector("#name").innerHTML = monster.name;
+            intervalSlider.value = monster.interval;
+            speedSlider.value = monster.rate;
+            volumeSlider.value = monster.volume;
+            loopSwitch.value = monster.loop;
+
+            document.querySelector("#intervalnum").innerHTML = intervalSlider.value;
+            document.querySelector("#volumenum").innerHTML = volumeSlider.value;
+            document.querySelector("#ratenum").innerHTML = speedSlider.value;
+        }
+    }
+}
+
+function changeMon(){
+    const word = this;
+    currentMonster = word;
 }
 
 function playSound(audio){
